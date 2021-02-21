36a73fae816f90d66552d7f2770d535b79326557
Al-Rikabi Ammar <ammarsami1986@gmail.com>
2021-02-21 21:12:18 +0000

    2 example

---
+++ introduction.js
@@ -0,0 +1,20 @@
+// ### Number sum
+
+// # 1
+
+let total1 = 0, count1 = 1;
+while (count1 <= 10) {
+  total1 += count1;
+  count1 += 1;
+}
+console.log(total1);
+
+
+
+// # 2
+let total2 = 0;
+for(let i=0; i <= 10; i++){
+    total2 += i;
+}
+console.log(total2);
+
